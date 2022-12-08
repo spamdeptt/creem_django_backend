4ffa109a54764477ad71fa9523cf919c03fb014f
Gaurav Sharma <gaurav.coolvirgo@gmail.com>
2022-12-08 09:06:39 +0530

    used viewsets and router

--- Quizapp2/urls.py
+++ Quizapp2/urls.py
@@ -1,19 +1,14 @@
 from django.urls import path
 from . import views
+from rest_framework.routers import SimpleRouter
 
-urlpatterns = [
-    path("creamcards/", views.CreamCardsList.as_view()),
-    path("creamcards/fresh_2", views.FreshCreamCards_delta2.as_view()),
-    path("creamcards/<int:pk>", views.CreamCardDetail.as_view()),
+router = SimpleRouter()
+router.register('creamcards', views.CreamCardsViewSet)
+router.register('questions', views.QuizQuestionsViewSet)
+router.register('q_collection', views.QuizQuestionsCollectionViewSet)
+router.register('ccfresh', views.FreshCreamCardsViewSet)
+router.register('correct', views.QuizQuestionCorrectViewSet)
+router.register('incorrect', views.QuizQuestionIncorrectViewSet)
 
-    path('questions/', views.QuizQuestionsList.as_view()),
-    path('questions/<int:pk>', views.QuizQuestionDetail.as_view()),
-
-    path('questions/collection/', views.QuizQuestionCollectionList.as_view()),
-    path('questions/collection/<int:pk>', views.QuizQuestionCollectionDetail.as_view()),
-    
-    path('questions/correct/<int:pk>', views.QuizQuestionsCorrect.as_view()),
-    path('questions/incorrect/<int:pk>', views.QuizQuestionsIncorrect.as_view()),
-    
-]
+urlpatterns = router.urls
 
--- Quizapp2/views.py
+++ Quizapp2/views.py
@@ -2,50 +2,33 @@ from .models import CreamCards, QuizQuestion, QuizQuestionCollection
 from .serializers import CreamCardsSerializer,QuizQuestionSerializers, QuizQuestionCollectionSerializers
 from rest_framework import generics
 from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
 from datetime import timedelta
 from django.utils import timezone
 from django.db.models import F
 
 
-class CreamCardsList(generics.ListCreateAPIView):
+class CreamCardsViewSet(ModelViewSet):
     queryset = CreamCards.objects.select_related('subject').select_related('author').all().order_by('-created_at')
     serializer_class = CreamCardsSerializer
 
-class FreshCreamCards_delta2(generics.ListAPIView):
+class FreshCreamCardsViewSet(ModelViewSet):
+    some_day_last_week = timezone.now().date() - timedelta(days = 3)
+    queryset = CreamCards.objects.select_related('subject').select_related('author').filter(created_at__gte=some_day_last_week).order_by('-created_at') #https://stackoverflow.com/questions/11205096/how-to-retrieve-records-from-past-weeks-in-django
     serializer_class = CreamCardsSerializer
+    
 
-    def get_queryset(self):
-        some_day_last_week = timezone.now().date() - timedelta(days = 3)
-        queryset = CreamCards.objects.select_related('subject').select_related('author').filter(created_at__gte=some_day_last_week).order_by('-created_at') #https://stackoverflow.com/questions/11205096/how-to-retrieve-records-from-past-weeks-in-django
-        return queryset
-
-class CreamCardDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = CreamCards.objects.all()
-    serializer_class = CreamCardsSerializer
-
-
-class QuizQuestionsList(generics.ListCreateAPIView):
+class QuizQuestionsViewSet(ModelViewSet):
     queryset = QuizQuestion.objects.all()
     serializer_class = QuizQuestionSerializers
 
-class QuizQuestionDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = QuizQuestion.objects.all()
-    serializer_class = QuizQuestionSerializers
-
-class QuizQuestionCollectionList(generics.ListCreateAPIView):
+class QuizQuestionsCollectionViewSet(ModelViewSet):
     queryset = QuizQuestionCollection.objects.all()
     serializer_class = QuizQuestionCollectionSerializers
+    
 
-class QuizQuestionCollectionDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = QuizQuestionCollection.objects.all()
-    serializer_class = QuizQuestionCollectionSerializers
-
-
-
-
-
-class QuizQuestionsCorrect(generics.RetrieveUpdateDestroyAPIView):   #https://stackoverflow.com/questions/51736015/increment-visits-counter-from-django-rest
+class QuizQuestionCorrectViewSet(ModelViewSet):
     queryset = QuizQuestion.objects.all()
     serializer_class = QuizQuestionSerializers
 
@@ -55,7 +38,7 @@ class QuizQuestionsCorrect(generics.RetrieveUpdateDestroyAPIView):   #https://st
         serializer = self.get_serializer(instance)
         return Response(serializer.data)
 
-class QuizQuestionsIncorrect(generics.RetrieveUpdateDestroyAPIView):  #https://stackoverflow.com/questions/51736015/increment-visits-counter-from-django-rest
+class QuizQuestionIncorrectViewSet(ModelViewSet):
     queryset = QuizQuestion.objects.all()
     serializer_class = QuizQuestionSerializers
 
@@ -64,3 +47,24 @@ class QuizQuestionsIncorrect(generics.RetrieveUpdateDestroyAPIView):  #https://s
         QuizQuestion.objects.filter(pk=instance.id).update(inCorrectCount=F('inCorrectCount') + 1)
         serializer = self.get_serializer(instance)
         return Response(serializer.data)
+
+
+# class QuizQuestionsCorrect(generics.RetrieveUpdateDestroyAPIView):   #https://stackoverflow.com/questions/51736015/increment-visits-counter-from-django-rest
+#     queryset = QuizQuestion.objects.all()
+#     serializer_class = QuizQuestionSerializers
+
+#     def retrieve(self, request, *args, **kwargs):
+#         instance = self.get_object()
+#         QuizQuestion.objects.filter(pk=instance.id).update(correctCount=F('correctCount') + 1)
+#         serializer = self.get_serializer(instance)
+#         return Response(serializer.data)
+
+# class QuizQuestionsIncorrect(generics.RetrieveUpdateDestroyAPIView):  #https://stackoverflow.com/questions/51736015/increment-visits-counter-from-django-rest
+#     queryset = QuizQuestion.objects.all()
+#     serializer_class = QuizQuestionSerializers
+
+#     def retrieve(self, request, *args, **kwargs):
+#         instance = self.get_object()
+#         QuizQuestion.objects.filter(pk=instance.id).update(inCorrectCount=F('inCorrectCount') + 1)
+#         serializer = self.get_serializer(instance)
+#         return Response(serializer.data)
